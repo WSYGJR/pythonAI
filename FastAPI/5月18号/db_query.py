22659154112daf46318db34e0402916cb1fc525e
ysj <815024115@qq.com>
2026-05-18 23:42:26 +0800

    123

--- FastAPI/5月18号/db_query.py
+++ FastAPI/5月18号/db_query.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 # 修正数据库 URL
-ASYNC_DATABASE_URL = "mysql+aiomysql://root:123456@localhost:3306/products?charset=utf8"
+ASYNC_DATABASE_URL = "mysql+aiomysql://root:123456@localhost:3306/fastapi?charset=utf8"
 
 async_engine = create_async_engine(
     ASYNC_DATABASE_URL,
@@ -17,15 +17,17 @@ async_engine = create_async_engine(
 )
 
 class Base(DeclarativeBase):
+
     create_time: Mapped[datetime] = mapped_column(DateTime, default=func.now, comment="创建时间")
     update_time: Mapped[datetime] = mapped_column(DateTime, default=func.now, onupdate=func.now, comment="更新时间")
 
-class Products(Base):
-    __tablename__ = "products"
-    id: Mapped[int] = mapped_column(primary_key=True, comment="商品ID")
-    productname: Mapped[str] = mapped_column(String(255), comment="商品名称")
-    classify: Mapped[str] = mapped_column(String(255), comment="商品分类")
-    price: Mapped[float] = mapped_column(Float, comment="商品价格")
+class Book(Base):
+    __tablename__ = "book"
+    id: Mapped[int] = mapped_column(primary_key=True, comment="书籍ID")
+    bookname: Mapped[str] = mapped_column(String(255), comment="书名")
+    author: Mapped[str] = mapped_column(String(255), comment="作者")
+    publisher: Mapped[str] = mapped_column(String(255), comment="出版社")
+    price: Mapped[float] = mapped_column(Float, comment="价格")
 
 async def create_table():
     async with async_engine.begin() as conn:
@@ -65,29 +67,63 @@ async def get_database():
         finally:
             await session.close()#关闭会话
 
+#普通查询
+# @app.get("/book/books")
+# async def get_book_list(db: AsyncSession = Depends(get_database)):
+#     #查询
+#     result = await db.execute(select(Book))#查询，返回一个ORM对象
+#     book = result.scalars().all() #查询所有
+#     book =  result.scalars().first()#获取第一个
+#     book = await db.get(Book, 3)#获取单条数据，根据主键查询
+#     return book
+
+
 #条件查询-比较判断
-@app.get("/products/search_product")
-async def get_product_list(product_price_min:float,product_price_max:float,db:AsyncSession = Depends(get_database)):
-    result = await db.execute(select(Products).where((Products.price >= product_price_min)&(Products.price <= product_price_max)&(Products.classify.like("%手机%"))))
-    products = result.scalars().all()
-    return products
+@app.get("/book/get_book/{book_id}")
+async def get_book_list(book_id:int,db:AsyncSession = Depends(get_database)):
+    result = await db.execute(select(Book).where(Book.id >= book_id))
+    book = result.scalar_one_or_none()
+    return book
+
+#条件查询-比较判断
+@app.get("/book/search_book")
+async def get_book_list(book_price:int,db:AsyncSession = Depends(get_database)):
+    result = await db.execute(select(Book).where(Book.price >= book_price))
+    book = result.scalars().all()
+    return book
+
+#条件查询-模糊查询
+@app.get("/book/search_book_like")
+async def get_book_list_like(db:AsyncSession = Depends(get_database)):
+    # result = await db.execute(select(Book).where(Book.author.like("曹%")))
+    # result = await db.execute(select(Book).where((Book.author.like("曹%")) | (Book.price >= 50)))
+    #查数据库里的id如果在书籍id列表内就返回
+    id_list = [1,3,5,7,9]
+    result = await db.execute(select(Book).where((Book.id.in_(id_list))))
+    book = result.scalars().all()
+    return book
 
 #聚合查询
-@app.get("/products/count")
+@app.get("/book/count")
 async def get_count(db:AsyncSession = Depends(get_database)):
-    result = await db.execute(select(func.count(Products.id)))
+    result = await db.execute(select(func.count(Book.id)))
     count = result.scalar()
     return count
 
+@app.get("/book/max")
+async def get_max(db:AsyncSession = Depends(get_database)):
+    result = await db.execute(select(func.max(Book.price)))
+    count = result.scalar()
+    return count
 
 #分页查询
-@app.get("/products/get_products")
-async def get_products(
+@app.get("/book/get_books")
+async def get_books(
         page:int = 1,
         page_size:int = 3,
         db:AsyncSession = Depends(get_database)
 ):
     skip = (page - 1) * page_size
-    result = await db.execute(select(Products).offset(skip).limit(page_size))
+    result = await db.execute(select(Book).offset(skip).limit(page_size))
     books = result.scalars().all()
     return books
